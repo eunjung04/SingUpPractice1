08e0c5be38751f3fa8bed2ce73d580777d3710be
eunjung04 <peunj04@gmail.com>
2020-04-14 20:50:58 +0900

    처음

--- app/src/main/java/com/example/singuppractice/BasicActivity.kt
+++ app/src/main/java/com/example/singuppractice/BasicActivity.kt
@@ -1,7 +1,15 @@
 package com.example.singuppractice
 
+import androidx.appcompat.app.AppCompatActivity
+
 //프로젝트에서 모든 액티비티가 공통적으로 사용하는
 //변수 or 함수들을 모아두는 클래스
 
-abstract class BasicActivity {
+abstract class BasicActivity : AppCompatActivity(){
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
 }
--- app/src/main/java/com/example/singuppractice/MainActivity.kt
+++ app/src/main/java/com/example/singuppractice/MainActivity.kt
@@ -3,10 +3,24 @@ package com.example.singuppractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BasicActivity() {
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 }
+
+
